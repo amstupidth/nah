8e719ebd43c9246f5e7bfe7ad80131470a3d4855
amstupidth <amstupidth@users.noreply.github.com>
2017-07-07 14:42:06 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SK Inter Food</title>
+<meta charset="UTF-8">
+<?php require 'style.php'; ?>  
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50" class="pre-scrollable">
+</body>
+</html>
